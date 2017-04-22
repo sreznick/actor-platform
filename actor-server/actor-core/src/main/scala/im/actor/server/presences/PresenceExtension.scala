30b04f00580f486f294a4f7a2e523423e68d63fd
Sergey Reznick <Sergey.Reznick@gmail.com>
2017-04-22 15:31:14 +0300

    Small refactoring

--- actor-server/actor-core/src/main/scala/im/actor/server/presences/PresenceExtension.scala
+++ actor-server/actor-core/src/main/scala/im/actor/server/presences/PresenceExtension.scala
@@ -7,7 +7,13 @@ import akka.util.Timeout
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-sealed trait PresenceExtension extends Extension
+trait PresenceExtension extends Extension {
+  def subscribe(userId: Int, consumer: ActorRef): Future[Unit]
+  def subscribe(userIds: Set[Int], consumer: ActorRef): Future[Unit]
+  def unsubscribe(userId: Int, consumer: ActorRef): Future[Unit]
+  def presenceSetOnline(userId: Int, authId: Long, timeout: Long): Unit
+  def presenceSetOffline(userId: Int, authId: Long, timeout: Long): Unit
+}
 
 class PresenceExtensionImpl(system: ActorSystem) extends PresenceExtension {
   import PresenceManager._
@@ -17,22 +23,22 @@ class PresenceExtensionImpl(system: ActorSystem) extends PresenceExtension {
 
   private val region = PresenceManagerRegion.startRegion()(system)
 
-  def subscribe(userId: Int, consumer: ActorRef): Future[Unit] = {
+  override def subscribe(userId: Int, consumer: ActorRef): Future[Unit] = {
     region.ref.ask(Envelope(userId, Subscribe(consumer))).mapTo[SubscribeAck].map(_ ⇒ ())
   }
 
-  def subscribe(userIds: Set[Int], consumer: ActorRef): Future[Unit] =
+  override def subscribe(userIds: Set[Int], consumer: ActorRef): Future[Unit] =
     Future.sequence(userIds map (subscribe(_, consumer))) map (_ ⇒ ())
 
-  def unsubscribe(userId: Int, consumer: ActorRef): Future[Unit] = {
+  override def unsubscribe(userId: Int, consumer: ActorRef): Future[Unit] = {
     region.ref.ask(Envelope(userId, Unsubscribe(consumer))).mapTo[UnsubscribeAck].map(_ ⇒ ())
   }
 
-  def presenceSetOnline(userId: Int, authId: Long, timeout: Long): Unit = {
+  override def presenceSetOnline(userId: Int, authId: Long, timeout: Long): Unit = {
     region.ref ! Envelope(userId, UserPresenceChange(Online, authId, timeout))
   }
 
-  def presenceSetOffline(userId: Int, authId: Long, timeout: Long): Unit = {
+  override def presenceSetOffline(userId: Int, authId: Long, timeout: Long): Unit = {
     region.ref ! Envelope(userId, UserPresenceChange(Offline, authId, timeout))
   }
 }
